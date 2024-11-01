1894e1fdec443b80d07ea38483b2b7986048e955
vpraneeth1507 <vpraneeth1507@gmail.com>
2024-11-01 21:58:22 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import Login from './pages/Login';
+import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './Header';
 
 function App() {
   return (
